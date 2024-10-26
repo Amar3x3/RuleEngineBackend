f43e2a4b3549c2544e06b32406bbd3931d8e07d9
Amar3x3 <amar3152002@gmail.com>
2024-10-26 12:15:55 +0530

    allowed origins to alllowed origin patterns

--- src/main/java/com/example/RuleEngineBackend/configuration/WebConfig.java
+++ src/main/java/com/example/RuleEngineBackend/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // Allow all endpoints
-                        .allowedOrigins("*")  // Allow requests from this origin
+                        .allowedOriginPatterns("*")    // Allow requests from this origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow these HTTP methods
                         .allowedHeaders("*")  // Allow all headers
                         .allowCredentials(true);  // Allow credentials such as cookies
